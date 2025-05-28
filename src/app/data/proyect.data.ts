dffd417a997bd783021fc573a97e2b63d67d0d82
Jhonsagui81 <3634733270101@ingenieria.usac.edu.gt>
2025-05-27 23:52:50 -0600

    feat: new projects

--- src/app/data/proyect.data.ts
+++ src/app/data/proyect.data.ts
@@ -39,6 +39,68 @@ export const PROJECTS: Proyect[] = [
           { main: 'assets/SaludPlus/despliegue.png', thumbnail: 'assets/SaludPlus/despliegue.png' },
         ],
         codigoFuente: 'https://gitlab.com/ayds1_g8/AYDS1_G8.git'
+    },
+    {
+        id: 2,
+        title: 'Olimpiadas USAC',
+        description: 'Sistema distribuido en Kubernetes (GCP) para monitorear en tiempo real las competencias de Natación, Boxeo y Atletismo entre las facultades de Ingeniería y Agronomía. Utiliza Grafana para visualización y Kafka para streaming de datos.',
+        technologies: [
+            {icon:"assets/SaludPlus/tecnol/K8.svg", name:"Kubernetes"},
+            {icon:"assets/SaludPlus/tecnol/gcp.svg", name:"gcloud"},
+            {icon:"assets/SaludPlus/tecnol/rust.svg", name:"Rust"},
+            {icon:"assets/SaludPlus/tecnol/go.svg", name:"Go"},
+            {icon:"assets/SaludPlus/tecnol/docker.svg", name:"Docker"},
+            {icon:"assets/SaludPlus/tecnol/apache-kafka.svg", name:"Kafka"},
+            {icon:"assets/SaludPlus/tecnol/redis.svg", name:"Redis"},
+            {icon:"assets/SaludPlus/tecnol/grafana.svg", name:"Grafana"},
+        ],
+        images: [
+          { main: 'assets/Olimpiada/image.png', thumbnail: 'assets/Olimpiada/image.png' },
+          { main: 'assets/Olimpiada/image.png', thumbnail: 'assets/Olimpiada/image.png' },
+          { main: 'assets/Olimpiada/image.png', thumbnail: 'assets/Olimpiada/image.png' },
+          { main: 'assets/Olimpiada/image.png', thumbnail: 'assets/Olimpiada/image.png' }
+        ],
+        codigoFuente: 'https://github.com/Jhonsagui81/SIstemas-Operativos/tree/46247c3b03d7a2833c6e8713910c408d524ae444/Proyecto2'
+    },
+    {
+        id: 3,
+        title: 'TaskFlow + CloudDrive',
+        description: 'Aplicación web full-stack que integra gestión de tareas y almacenamiento de archivos en la nube, implementada de forma paralela en AWS y Azure para comparar servicios equivalentes. Destaca por su arquitectura resiliente, escalabilidad bajo demanda y despliegue 100% en la nube',
+        technologies: [
+            {icon:"assets/SaludPlus/tecnol/node.svg", name:"Node.js"},
+            {icon:"assets/SaludPlus/tecnol/C#.svg", name:"C#"},
+            {icon:"assets/SaludPlus/tecnol/type.svg", name:"Typescript"},
+            {icon:"assets/SaludPlus/tecnol/aws.svg", name:"AWS"},
+            {icon:"assets/SaludPlus/tecnol/azure.svg", name:"Azure"}
+        ],
+        images: [
+          { main: 'assets/Task/1.png', thumbnail: 'assets/Task/1.png' },
+          { main: 'assets/Task/2.png', thumbnail: 'assets/Task/2.png' },
+          { main: 'assets/Task/3.png', thumbnail: 'assets/Task/3.png' },
+          { main: 'assets/Task/4.png', thumbnail: 'assets/Task/4.png' },
+          { main: 'assets/Task/5.png', thumbnail: 'assets/Task/5.png' },
+          { main: 'assets/Task/6.png', thumbnail: 'assets/Task/6.png' },
+
+        ],
+        codigoFuente: 'https://github.com/JPorres03/Semi1-A-1S2025-Grupo4-Proyecto/tree/f88081e072c6fad19ff75079eca23032e87e7cbe/Practica%202'
+    },
+    {
+        id: 4,
+        title: 'QueryCrypter',
+        description: 'QueryCrypter es un intérprete que emula un motor de bases de datos en memoria, diseñado para procesar instrucciones SQL y operaciones programáticas avanzadas. Permite ejecutar consultas clásicas (creación, inserción, actualización y eliminación de tablas) e integra características propias de lenguajes de programacion como manejo de variables, estructuras de control y funciones personalizadas',
+        technologies: [
+            {icon:"assets/SaludPlus/tecnol/node.svg", name:"Node.js"},
+            {icon:"assets/SaludPlus/tecnol/type.svg", name:"Typescript"},
+            {icon:"assets/SaludPlus/tecnol/react.svg", name:"React"},
+            {icon:"assets/SaludPlus/tecnol/graphviz.svg", name:"Graphviz"},
+        ],
+        images: [
+          { main: 'assets/interprete/1.png', thumbnail: 'assets/interprete/1.png' },
+          { main: 'assets/interprete/4.png', thumbnail: 'assets/interprete/4.png' },
+          { main: 'assets/interprete/5.png', thumbnail: 'assets/interprete/5.png' },
+          { main: 'assets/interprete/6.png', thumbnail: 'assets/interprete/6.png' },
+        ],
+        codigoFuente: 'https://github.com/Jhonsagui81/OLC1_Proyecto2_202106003.git'
     }
 ]
 
